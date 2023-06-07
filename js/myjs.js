b6907ff2c4fc106c679dbe7df637c198710b9770
jaquinterocl <jaquinterocl@ufpso.edu.co>
2023-06-07 03:31:24 -0500

    menu desplazable

--- js/myjs.js
+++ js/myjs.js
@@ -1,7 +1,14 @@
 const icononav = document.querySelector('.icono');
 const barranav = document.querySelector('.barra');
+const iconoi = document.querySelector(".icono i");
+var can = document.getElementById("cambiodecolor")
 
-icononav.onclick() = function (){
+icononav.onclick = function (){
    barranav.classList.toggle('open');
-   const openyes = barranav.classList.contains('open');
+   const openyes = barranav.classList.contains("open");
+   can.classList.toggle("open");
+   iconoi.classList = openyes
+   ? "fa-solid fa-bars fa-2xl"
+   : "fa-solid fa-bars fa-2xl"
+ 
 }
